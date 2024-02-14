dcdfebd8181d3181b061f9e87fa2ae6e47f1a375
alejandrom12 <alejandro.morel1905@gmail.com>
2024-02-14 14:39:14 -0300

    feat: add hero section

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className='pt-40 pl-40 mt-5 hero'>
+      <h1>Un libro es una ventana al mundo</h1>
+      <p className='mt-6 mb-10'>
+        Despierta tus sueños leyendo un libro cada día
+      </p>
+      <a href='#' className='button-cta'>
+        Reserva ahora
+      </a>
+    </div>
+  );
 };
 
 export default Hero;
