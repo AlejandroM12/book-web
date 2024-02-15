5019e6593ac49de3045aeae45e0ffc8c646b002b
alejandrom12 <alejandro.morel1905@gmail.com>
2024-02-15 13:50:41 -0300

    feat: add testimonial section

--- src/components/Testimonials/index.js
+++ src/components/Testimonials/index.js
@@ -1,7 +1,67 @@
+import Image from 'next/image';
 import React from 'react';
+import { Client1Img, Client2Img, Client3Img, KutipImg } from '../../../public';
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section>
+      <div className='pt-10 mt-20'>
+        <div className='grid grid-cols-3 gap-3'>
+          {/* col */}
+          <div className='text-center border-2 testimonial'>
+            <div>
+              <div className='flex items-center justify-center text-center'>
+                <Image
+                  src={Client1Img}
+                  className='ml-32 mr-20 gambar'
+                  alt='client img'
+                />
+                <Image src={KutipImg} className='gambar' alt='kutip img' />
+              </div>
+              <h5>Eka Ardilah</h5>
+              <p>
+                Me siento muy útil con todos los libros proporcionados, así que
+                hago mi trabajo fácilmente y felizmente.
+              </p>
+            </div>
+          </div>
+          {/* col */}
+          {/* col */}
+          <div className='text-center border-2 testimonial'>
+            <div>
+              <div className='flex items-center justify-center text-center'>
+                <Image
+                  src={Client2Img}
+                  className='ml-32 mr-20 gambar'
+                  alt='client img'
+                />
+                <Image src={KutipImg} className='gambar' alt='kutip img' />
+              </div>
+              <h5>Hazar Hamzah</h5>
+              <p>Gracias. Muchos buenos libros para acompañar cada día.</p>
+            </div>
+          </div>
+          {/* col */}
+          {/* col */}
+          <div className='text-center border-2 testimonial'>
+            <div>
+              <div className='flex items-center justify-center text-center'>
+                <Image
+                  src={Client3Img}
+                  className='ml-32 mr-20 gambar'
+                  alt='client img'
+                />
+                <Image src={KutipImg} className='gambar' alt='kutip img' />
+              </div>
+              <h5>Khoerunnisa</h5>
+              <p>Gracias equipo, muchos libros recomendados para la lectura.</p>
+            </div>
+          </div>
+          {/* col */}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
