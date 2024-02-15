53d683b29a13f1630e820ff24eb3defeaff499c5
alejandrom12 <alejandro.morel1905@gmail.com>
2024-02-15 13:37:27 -0300

    feat: add suscribe section

--- src/components/Suscribe/index.js
+++ src/components/Suscribe/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Suscribe = () => {
-  return <div>Suscribe</div>;
+  return (
+    <section className='flex items-center justify-center mt-20 subscribe'>
+      <div>
+        <h4>Libros recomendados para que los revises</h4>
+        <form>
+          <input
+            placeholder='Escribe tu recomendación'
+            className='submit-area'
+          />
+          <button className='submit-button'>Enviar</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Suscribe;
